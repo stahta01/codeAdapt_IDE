e082d4f79fd248b48cc2edfa94502c030417a7c2
Tim S <stahta01@users.sourceforge.net>
2016-05-13 21:15:03 -0400

    * build: Removed file "sdk/nullptr.cpp" and added option "-std=gnu++11".

--- src/sdk/nullptr.cpp
+++ src/sdk/nullptr.cpp
@@ -3,5 +3,6 @@
 /* -----------------------------------------------
  * remove this once the compiler supports C++0x
  * ----------------------------------------------- */
-
+#if 0
 null_pointer_t nullptr;
+#endif
