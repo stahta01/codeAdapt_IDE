4ce78f9807227c36561e8f5d9a890b287c934adf
Tim S <stahta01@users.sourceforge.net>
2016-05-14 13:27:57 -0400

    * autorevision.h: Added file with SVN number 4907.

---
+++ src/include/autorevision.h
@@ -0,0 +1,18 @@
+/*4907*/
+//don't include this header, only configmanager-revision.cpp should do this.
+#ifndef AUTOREVISION_H
+#define AUTOREVISION_H
+
+
+#include <wx/string.h>
+
+namespace autorevision
+{
+	const unsigned int svn_revision = 4907;
+	const wxString svnRevision(_T("4907"));
+	const wxString svnDate(_T("unknown date"));
+}
+
+
+
+#endif
