217415468f0ed4380b5cc05ea07233fb2ce9b61a
Tim S <stahta01@users.sourceforge.net>
2016-05-15 18:01:59 -0400

    * headerfixup: Replaced method cbGetTextFromUser with caGetTextFromUser.

--- src/plugins/contrib/headerfixup/configuration.cpp
+++ src/plugins/contrib/headerfixup/configuration.cpp
@@ -246,7 +246,7 @@ void Configuration::OnApply()
 
 void Configuration::OnBtnAddGroupClick(wxCommandEvent& /*event*/)
 {
-  wxString GroupName = ::cbGetTextFromUser(_("Enter name for new group"));
+  wxString GroupName = ::caGetTextFromUser(_("Enter name for new group"));
   if ( GroupName.IsEmpty() )
     return;
 
@@ -298,7 +298,7 @@ void Configuration::OnRenameGroup(wxCommandEvent& /*event*/)
   if ( GroupName.IsEmpty() )
     return;
 
-  GroupName = ::cbGetTextFromUser(_("Enter new group name"),_("Change group name"),GroupName);
+  GroupName = ::caGetTextFromUser(_("Enter new group name"),_("Change group name"),GroupName);
   if ( GroupName.IsEmpty() )
     return;
 
@@ -354,7 +354,7 @@ void Configuration::OnGroupsSelect(wxCommandEvent& /*event*/)
 
 void Configuration::OnBtnAddIdentifierClick(wxCommandEvent& /*event*/)
 {
-  wxString Identifier = ::cbGetTextFromUser(_("Enter new identifier"));
+  wxString Identifier = ::caGetTextFromUser(_("Enter new identifier"));
   if ( Identifier.IsEmpty() )
     return;
 
@@ -403,7 +403,7 @@ void Configuration::OnChangeIdentifier(wxCommandEvent& /*event*/)
   if ( Identifier.IsEmpty() )
     return;
 
-  Identifier = ::cbGetTextFromUser(_("Enter new identifier"),_("Change identifier"),Identifier);
+  Identifier = ::caGetTextFromUser(_("Enter new identifier"),_("Change identifier"),Identifier);
   if ( Identifier.IsEmpty() )
     return;
 
