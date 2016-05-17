66914d494574f84ff2188c94b672dd404bac52aa
Tim S <stahta01@users.sourceforge.net>
2016-05-16 20:29:52 -0400

    * build: Changed "share\codeblocks" to "share\codeadapt".

--- src/plugins/contrib/ToolsPlus/update.bat
+++ src/plugins/contrib/ToolsPlus/update.bat
@@ -1,6 +1,6 @@
 @echo off
-md ..\..\..\devel\share\CodeBlocks\images\settings > nul 2>&1
-md ..\..\..\output\share\CodeBlocks\images\settings > nul 2>&1
-copy .\Resources\*.png ..\..\..\devel\share\CodeBlocks\images\settings\ > nul 2>&1
-copy .\Resources\*.png ..\..\..\output\share\CodeBlocks\images\settings\ > nul 2>&1
+md ..\..\..\devel\share\codeadapt\images\settings > nul 2>&1
+md ..\..\..\output\share\codeadapt\images\settings > nul 2>&1
+copy .\Resources\*.png ..\..\..\devel\share\codeadapt\images\settings\ > nul 2>&1
+copy .\Resources\*.png ..\..\..\output\share\codeadapt\images\settings\ > nul 2>&1
 exit 0
