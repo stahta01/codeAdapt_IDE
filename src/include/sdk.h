12f7aba94db6196ccd3ac481ed33e2798d7ae1fb
Tim S <stahta01@users.sourceforge.net>
2016-05-27 09:10:12 -0400

    * sdk: Edited sdk.h.

--- src/include/sdk.h
+++ src/include/sdk.h
@@ -1,15 +1,11 @@
-/** Code::Blocks SDK precompiled headers support for plugins.
-  * This is precompiled with -DBUILDING_PLUGIN
-*/
+/*
+ *  Code::Blocks SDK precompiled headers support for plugins.
+ *  This is precompiled with -DBUILDING_PLUGIN
+ */
 
 #ifndef SDK_H
 #define SDK_H
 
-#ifdef __WXMSW__
-	#include "sdk_common.h"
-#else
-	// for non-windows platforms, one PCH is enough
-	#include "sdk_precomp.h"
-#endif
+#include "sdk_common.h"
 
 #endif // SDK_H
