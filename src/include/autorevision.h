277d123b37cf0732ac5e9fdfd70ef5599d7097f0
Tim S <stahta01@users.sourceforge.net>
2016-08-02 12:35:02 -0400

    + sdk: Added autorevision.h with SVN of Zero.

---
+++ src/include/autorevision.h
@@ -0,0 +1,17 @@
+/*0*/
+#ifndef AUTOREVISION_H
+#define AUTOREVISION_H
+
+
+#include <wx/string.h>
+
+namespace autorevision
+{
+	const unsigned int svn_revision = 0;
+	const wxString svnRevision(_T("0"));
+	const wxString svnDate(_T("2000-01-01"));
+}
+
+
+
+#endif
