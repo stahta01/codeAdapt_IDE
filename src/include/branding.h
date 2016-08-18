fd9d702adee63c70e4e470bd5287423a27eaf1fe
Tim S <stahta01@users.sourceforge.net>
2016-08-18 09:11:06 -0400

    * sdk: Changed STANDARD_DATA_PATH to "/share/codeadapt".

--- src/include/branding.h
+++ src/include/branding.h
@@ -6,7 +6,7 @@
 
 #define DDE_SERVICE        _T("CODEBLOCKS")
 #define DDE_TOPIC          _T("CodeBlocksDDEServer")
-#define STANDARD_DATA_PATH "/share/codeblocks"
+#define STANDARD_DATA_PATH "/share/codeadapt"
 #define APP_NAME           "CodeBlocks"
 #define APP_LOG_FILENAME   "codeblocks.log"
 #define APP_CONFIG         "CodeBlocksConfig"
