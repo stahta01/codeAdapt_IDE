f2727e1d546edac538f508ece1204514d28a7a45
Tim S <stahta01@users.sourceforge.net>
2016-08-17 21:37:11 -0400

    * sdk: Added defines to "branding.h".

--- src/include/branding.h
+++ src/include/branding.h
@@ -4,7 +4,11 @@
 #ifndef BRANDING_H
 #define BRANDING_H
 
+#define DDE_SERVICE        _T("CODEBLOCKS")
+#define DDE_TOPIC          _T("CodeBlocksDDEServer")
 #define STANDARD_DATA_PATH "/share/codeblocks"
 #define APP_NAME           "CodeBlocks"
+#define APP_LOG_FILENAME   "codeblocks.log"
+#define APP_CONFIG         "CodeBlocksConfig"
 
 #endif // BRANDING_H
